876cf4001d9af95b87492495431fe4b7b2001712
JR-Morgan <jeddrmorgan@gmail.com>
2019-10-19 03:23:19 +0100

    Created Tile, TileMap, and TileSet classes

---
+++ TeamProject/Engine/Engine/TileGrid/Tile.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.TileGrid
+{
+    /// <summary>
+    /// This class reperesents a Tile Map Tile Game Object
+    /// </summary>
+    public class Tile : GameObject, IDrawable
+    {
+        private readonly Texture2D texture;
+        private readonly Vector2 size;
+
+        public Tile(Texture2D texture, Vector2 Position, Vector2 size) : base(Position)
+        {
+            this.texture = texture;
+            this.size = size;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, new Rectangle(position.ToPoint(), size.ToPoint()), Color.White);
+        }
+    }
+}
---
+++ TeamProject/Engine/Engine/TileGrid/TileSet.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.TileGrid
+{
+    /// <summary>
+    /// This is a class for a tile set
+    /// </summary>
+    public class TileSet
+    {
+        public readonly List<Texture2D> tileSetTextures;
+        public readonly Vector2 textureSize;
+
+        public TileSet(List<Texture2D> tileSetTextures, Vector2 textureSize)
+        {
+            this.tileSetTextures = tileSetTextures;
+            this.textureSize = textureSize;
+        }
+
+    }
+}
