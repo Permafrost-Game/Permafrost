e51494e6ca1c34452884774022298717cbcdc257
JR-Morgan <jeddrmorgan@gmail.com>
2019-10-21 18:31:12 +0100

    Reverterd 895d171c3dea8ed10f5a9e75d1124a3a49ee032b
    Removed Content Pipeline project. This can be added later but its easier for now just reading map files without it.

---
+++ TeamProject/ContentPipeline/TileMap/TileMapImporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentPipeline
+{
+    [ContentImporter(".tmx", DefaultProcessor = "TiledMapProcessor", DisplayName = "TileMap Importer - ContentPipeline.TileMap")]
+    public class TiledMapImporter : ContentImporter<SerialisedTileMap>
+    {
+    }
+}
