156e94d2c4a907d949688dd00d924c063bfb47ac
JR-Morgan <jeddrmorgan@gmail.com>
2019-11-20 21:54:06 +0000

    Added  Resource Classes

---
+++ TeamProject/GlobalWarmingGame/ResourceItems/ResourceItem.cs
@@ -0,0 +1,15 @@
+﻿namespace GlobalWarmingGame.ResourceItems
+{
+    public class ResourceItem
+    {
+        public ResourceType Type { get; set; }
+        public int Ammount { get; set; }
+
+        private ResourceItem(ResourceType Type, int Ammount = 0)
+        {
+            this.Type = Type;
+            this.Ammount = Ammount;
+        }
+
+    }
+}
---
+++ TeamProject/GlobalWarmingGame/ResourceItems/ResourceType.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GlobalWarmingGame.ResourceItems
+{
+    public class ResourceType
+    {
+        private Texture2D texture;
+        public string ID { get; private set; }
+        public string DisplayName { get; private set; }
+        public string Description { get; private set; }
+
+        public ResourceType(string id, string displayName, string description, Texture2D texture)
+        {
+            this.ID = id;
+            this.DisplayName = displayName;
+            this.Description = description;
+            this.texture = texture;
+        }
+
+    }
+}
