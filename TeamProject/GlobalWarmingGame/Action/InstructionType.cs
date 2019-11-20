c48f3a6efd6dd292296fa3cdc2533cccb77c6b1a
JR-Morgan <jeddrmorgan@gmail.com>
2019-11-20 19:50:23 +0000

    Added Comments to InstructionType
    Added commends and changed the constructor.

--- TeamProject/GlobalWarmingGame/Action/InstructionType.cs
+++ TeamProject/GlobalWarmingGame/Action/InstructionType.cs
@@ -12,24 +12,19 @@
         public delegate void Action();
         private Action action;
 
-        public InstructionType(string id, string name, string description, Action action)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id">Unique ID</param>
+        /// <param name="name">Display name</param>
+        /// <param name="description">Display description</param>
+        /// <param name="action">Method that should be called on <see cref="InstructionType.Act"/></param>
+        public InstructionType(string id, string name, string description, Action action = default)
         {
             this.ID = id;
             this.Name = name;
             this.Description = description;
             this.action = action;
-        }
-        /// <summary>
-        /// Creates a new InstructionType with no action
-        /// </summary>
-        /// <param name="id">Unique ID</param>
-        /// <param name="name">Display Name</param>
-        /// <param name="description">Display Description</param>
-         public InstructionType(string id, string name, string description)
-        {
-            this.ID = id;
-            this.Name = name;
-            this.Description = description;
         }
 
 
