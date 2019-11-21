edadcf782dc173b1ce759aacf3ac3cb71bdb01a1
JohnHamilton <john.crhamilton@gmail.com>
2019-11-21 19:24:52 +0000

    Update PathFinder.cs
    -Pathfinder getAdjacentTiles should now have a O(1) complexity.

--- TeamProject/Engine/Engine/PathFinding/PathFinder.cs
+++ TeamProject/Engine/Engine/PathFinding/PathFinder.cs
@@ -18,7 +18,7 @@ namespace Engine.PathFinding
             Tile goalT = tileMap.GetTileAtPosition(goal);
             return Find(startT, goalT, canPassObstacles);
         }
-        
+
         /// <summary>
         /// Takes a start tile, end tile and a bool if the pathfinding should consider obsticles
         /// </summary>
@@ -36,11 +36,11 @@ namespace Engine.PathFinding
             ///<summary>While you still have places to look</summary>
             while (openDictionary.Count != 0)
             {
-                
+
                 Tile current = null;
 
                 //Lowest is a number that shouldn't be a possible total cost in map
-                double lowest = 1000000; 
+                double lowest = 1000000;
 
                 TileCosts lowestTileCost = null;
 
@@ -49,7 +49,7 @@ namespace Engine.PathFinding
                 {
                     if (i.GetTotalCost() < lowest)
                     {
-                  
+
                         lowest = i.GetTotalCost();
                         lowestTileCost = i;
 
@@ -89,7 +89,7 @@ namespace Engine.PathFinding
 
                     //Else we have potentially found a shorter path from the start to this neighbour tile in the open list
                     else
-                    { 
+                    {
                         //Version of neighbour already in the open dictionary
                         Tile openNeighbour = openDictionary.FirstOrDefault(x => x.Key.Equals(neighbour)).Key;
 
@@ -113,29 +113,23 @@ namespace Engine.PathFinding
         /// <summary>
         /// Look at all the map tiles and return the adjacent tiles
         /// </summary>
-        /// <returns> Return a list of adjacent tiles </returns>
-        private static Dictionary<Tile,TileCosts> GetAdjacentTiles(Tile current, Tile goal, TileCosts currentTotalCost, TileMap tMap, bool canPassOverObstacles)
+        /// <returns> list of adjacent tiles </returns>
+        private static Dictionary<Tile, TileCosts> GetAdjacentTiles(Tile current, Tile goal, TileCosts currentTotalCost, TileMap tMap, bool canPassOverObstacles)
         {
             //Temp adjacent tile list
             Dictionary<Tile, TileCosts> tileList = new Dictionary<Tile, TileCosts>();
-            double currentTileX = current.Position.X;
-            double currentTileY = current.Position.Y;
+            
+            int[] xDirections = new int[] { 1, -1, 0, 0, 1, -1, 1, -1 };
+            int[] yDirections = new int[] { 0, 0, 1, -1, 1, 1, -1, -1 };
 
-            foreach (Tile tile in tMap.Tiles)
+            //Loop through all 8 possible adjacent tiles
+            for (int i = 0; i < xDirections.Length; i++)
             {
-                double tileX = tile.Position.X;
-                double tileY = tile.Position.Y;
+                Tile tile = AdjacentTile(current, tMap, canPassOverObstacles, xDirections[i], yDirections[i]);
 
-                //If game object can't pass over game objects and tile is an obstacle
-                if (!canPassOverObstacles && !tile.Walkable)
+                //TileFinder will only return null if the tile is not passable or outside of the tilemap
+                if (tile != null)
                 {
-                    continue;
-                }
-
-                //Find the tiles that are diagonal, horizontal and vertical.
-                if ((DistanceCalculator(current,tile) == tile.size.X) || (DistanceCalculator(current, tile) == (tile.size.X * Math.Sqrt(2)))) 
-                {
-
                     //Distance from start to neighbour through current tile
                     double neighbourToStartCost = currentTotalCost.FromStart + DistanceCalculator(current, tile);
 
@@ -145,17 +139,47 @@ namespace Engine.PathFinding
                         Parent = current
                     };
 
-                    tileList.Add(tile, neighbourTileCosts); 
-                    continue;
+                    tileList.Add(tile, neighbourTileCosts);
                 }
             }
+
             return tileList;
+        }
+
+        /// <summary>
+        /// Find a tile within the map that is one tile away from the current tile 
+        /// </summary>
+        /// <param name="current"></param>
+        /// <param name="tMap"></param>
+        /// <param name="changeX"></param>
+        /// <param name="changeY"></param>
+        /// <returns> adjacent tile </returns>
+        private static Tile AdjacentTile(Tile current, TileMap tMap, bool canPassOverObstacles, int changeX, int changeY)
+        {
+            int x = (int)current.Position.X / (int)Math.Round(current.size.X);
+            int y = (int)current.Position.Y / (int)Math.Round(current.size.Y);
+
+            Tile t = null;
+            
+            //If tile is in the tile map
+            if ((x + changeX) >= 0 && (y + changeY) >= 0 && (x + changeX) < tMap.Tiles.GetLength(0) && (y + changeX) < tMap.Tiles.GetLength(1))
+            {
+                t = tMap.Tiles[(x + changeX), (y + changeY)];
+
+                //If game object can't pass over game objects and tile is an obstacle
+                if (!canPassOverObstacles && !t.Walkable)
+                {
+                    return null;
+                }
+            }
+
+            return t;
         }
 
         ///<summary>
         ///Calculate the distance of a tile to another tile
         ///</summary>
-        ///<returns>double toEndCost</returns>
+        ///<returns> double toEndCost </returns>
         private static double DistanceCalculator(Tile current, Tile end)
         {
             //Cost from this node to another node
@@ -167,7 +191,7 @@ namespace Engine.PathFinding
 
             //Standard distance formula: distance = sqrt((X2-X1)^2 + (Y2-Y1)^2)
             toEndCost = Math.Sqrt((currentVector.X - endVector2.X) * (currentVector.X - endVector2.X) + (currentVector.Y - endVector2.Y) * (currentVector.Y - endVector2.Y));
-            
+
             return toEndCost;
         }
 
@@ -176,13 +200,15 @@ namespace Engine.PathFinding
         /// Since the queue must have the starting tile at the front we have to push all of the tiles into a stack.
         /// Then pop the stack and enqueue all the tiles
         /// </summary>
-        /// <returns> Queue<Tile> </returns>
-        private static Queue<Vector2> BuildPath(Tile tile, Dictionary<Tile, TileCosts> closedTiles) {
+        /// <returns> Queue<Vector2> </returns>
+        private static Queue<Vector2> BuildPath(Tile tile, Dictionary<Tile, TileCosts> closedTiles)
+        {
 
             Stack<Vector2> tileStack = new Stack<Vector2>();
             Queue<Vector2> tileQueueFromStart = new Queue<Vector2>();
 
-            if (closedTiles != null) {
+            if (closedTiles != null)
+            {
 
                 //Current equals the end tile
                 Tile current = tile;
