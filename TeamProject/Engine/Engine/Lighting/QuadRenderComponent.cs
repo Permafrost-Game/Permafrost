314e0dc21ec12bbc9a8de946a0fe6d0246d76ebb
JR-Morgan <jeddrmorgan@gmail.com>
2019-12-01 22:23:03 +0000

    Added Licences for lighting files
    Added MIT licence for classes and files copyrighted by Catalin ZZ

--- TeamProject/Engine/Engine/Lighting/QuadRenderComponent.cs
+++ TeamProject/Engine/Engine/Lighting/QuadRenderComponent.cs
@@ -1,27 +1,35 @@
+/*#region Licence
+ *     Copyright (C) 2011 by Catalin Zima-Zegreanu
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”),
+    to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#endregion*/
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Engine.Lighting
 {
     /// <remarks>
-    /// This class was created by Catalin ZZ 
-    /// Source http://www.catalinzima.com/xna/samples/shader-based-dynamic-2d-smooth-shadows/
+    /// This class was created by Catalin ZZ and improved by
+    /// Original Source http://www.catalinzima.com/xna/samples/shader-based-dynamic-2d-smooth-shadows/
+    /// Improved Version http://www.funhazard.com/xna-resources.html
     /// </remarks>
-    public partial class QuadRenderComponent : DrawableGameComponent
+    public partial class QuadRenderComponent
     {
         VertexPositionTexture[] verts = null;
         short[] ib = null;
 
         // Constructor
-        public QuadRenderComponent(Game game)
-            : base(game)
+        public QuadRenderComponent()
         {
-            LoadContent();
-        }
-        
-        protected override void LoadContent()
-        {
-            verts = new VertexPositionTexture[]
+            this.verts = new VertexPositionTexture[]
             {
                 new VertexPositionTexture(
                     new Vector3(0,0,0),
@@ -38,17 +46,9 @@ namespace Engine.Lighting
             };
 
             ib = new short[] { 0, 1, 2, 2, 3, 0 };
-
-            base.LoadContent();
-        }
-        
-
-        protected override void UnloadContent()
-        {
-            base.UnloadContent();
         }
 
-        public void Render(Vector2 v1, Vector2 v2)
+        public void Render(GraphicsDevice graphics, Vector2 v1, Vector2 v2)
         {
             verts[0].Position.X = v2.X;
             verts[0].Position.Y = v1.Y;
@@ -62,8 +62,7 @@ namespace Engine.Lighting
             verts[3].Position.X = v2.X;
             verts[3].Position.Y = v2.Y;
 
-            GraphicsDevice.DrawUserIndexedPrimitives<VertexPositionTexture>(PrimitiveType.TriangleList, verts, 0, 4, ib, 0, 2);
+            graphics.DrawUserIndexedPrimitives<VertexPositionTexture>(PrimitiveType.TriangleList, verts, 0, 4, ib, 0, 2);
         }
-        
     }
 }
