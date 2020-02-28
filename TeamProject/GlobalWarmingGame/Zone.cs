3a87038d7198282190481c9a8669f741ffb57032
JR-Morgan <45512892+JR-Morgan@users.noreply.github.com>
2020-02-28 20:22:12 +0000

    Updated Tower generation Changed from every 10 to every 7 tiles Added water check for tower

--- TeamProject/GlobalWarmingGame/Zone.cs
+++ TeamProject/GlobalWarmingGame/Zone.cs
@@ -10,25 +10,48 @@ using Microsoft.Xna.Framework;
 
 namespace GlobalWarmingGame
 {
+
+
     class Zone
     {
+
+        private static readonly int towerDistance = 7;
+        private static readonly int towerSpan = 100;
+
         public List<GameObject> GameObjects { get; private set; }  
 
         private Zone(List<GameObject> objects)
         {
             GameObjects = objects;
         }
+
         public static Zone GenerateZone(int seed, TileMap tileMap, Vector2 zonePos)
         {
             List<GameObject> GameObjects = new List<GameObject>();
             Random rand = new Random(zonePos.GetHashCode());
-            if (((zonePos.X % 10 == 0) && zonePos.X < 100 && zonePos.X > -100) && ((zonePos.Y % 10 == 0) && zonePos.Y < 100 && zonePos.Y > -100) && ! zonePos.Equals(Vector2.Zero)) 
+            Vector2 zoneCenter = 32 *(GameObjectManager.ZoneMap.Size / 2);
+
+            if (   zonePos.X % towerDistance == 0
+                && zonePos.X < towerSpan
+                && zonePos.X > -towerSpan
+                && zonePos.Y % towerDistance == 0
+                && zonePos.Y < towerSpan
+                && zonePos.Y > -towerSpan
+                && !zonePos.Equals(Vector2.Zero)
+                && !tileMap.GetTileAtPosition(zoneCenter).Type.Equals("textures/tiles/main_tileset/water")
+                )
             {
-                GameObjects.Add((GameObject)InteractablesFactory.MakeInteractable(Interactable.Tower, new Vector2((32*50),(32*50))));
-                for (int i = rand.Next(0, 5); i < 5; i++) {
-                    GameObjects.Add((GameObject)InteractablesFactory.MakeInteractable(Interactable.Robot, new Vector2(((rand.Next(24, 40)) * 50), ((rand.Next(24, 40)) * 50))));
+                GameObjects.Add((GameObject)InteractablesFactory.MakeInteractable(Interactable.Tower, zoneCenter));
+
+                for (int i = 0; i < rand.Next(1, 5); i++) {
+                    GameObjects.Add((GameObject)InteractablesFactory.MakeInteractable(Interactable.Robot,
+                        tileMap.GetTileAtPosition(zoneCenter).Type.Equals("textures/tiles/main_tileset/water")?
+                        zoneCenter :
+                        zoneCenter + new Vector2(rand.Next(-128, 128), rand.Next(-128, 128))
+                        ));
                 }
             }
+
             foreach (Tile t in tileMap.Tiles)
             {
                 //int item = rand.Next(0, 100);
